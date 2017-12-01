1dd958cc87e67abfd20373e01fd28243a4c1f9f7
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-12-01 08:58:28 +0100

    Make plugin dependent on The Newsletter Plugin
    * Static method require_the_newsletter_plugin() to figure it all out
      from whatever clues we have
    * deactivate_plugins() on ourselves if the requirement is not met
    * Massage messages through $_GET["activate"] (which may need to be
      unset to prevent) and the admin_notices action

--- EPFL-newsletter.php
+++ EPFL-newsletter.php
@@ -18,13 +18,85 @@ class NewsletterConfig
 {
     static function hook ()
     {
-        register_activation_hook(__FILE__,
-                                 array(get_called_class(), 'activation_hook' ) );
+        add_action( 'admin_init', array(get_called_class(), 'require_the_newsletter_plugin' ));
     }
 
-    function activation_hook ()
+    /**
+     * Check that The Newsletter Plugin is available and activated.
+     *
+     * If not, deactivate ourselves and set up suitable red or green
+     * boxes at the top of the Extensions page to explain why.
+     */
+    static function require_the_newsletter_plugin ()
     {
-        wp_die( ___( 'EPFL-newsletter requires <a href="https://www.thenewsletterplugin.com/">the newsletter plugin</a> to be installed and activated.' ), __x( 'Error', 'die'), array( 'back_link' => true ) );
+        $the_newsletter_plugin = self::get_newsletter_plugin();
+        if (! $the_newsletter_plugin) {
+            self::add_admin_notice('error_need_newsletter_plugin');
+        } elseif (! is_plugin_active($the_newsletter_plugin)) {
+            // Assumption #1: EPFL-newsletter is active (otherwise
+            // this code wouldn't run)
+            // Assumption #2: we just entered this illegal state
+            // (otherwise we would have deactivated ourselves
+            // earlier). Therefore, either The Newsletter Plugin was
+            // just activated, or this plugin was just deactivated.
+            // Since we land here after a redirect, all PHP-side state
+            // is gone and the only clue that survives is
+            // $_GET["deactivate"]:
+            self::add_admin_notice(
+                $_GET["deactivate"] ?
+                'notice_newsletter_plugin_deactivated_must_deactivate' :
+                'error_newsletter_plugin_deactivated_cannot_activate');
+        } else {
+            return;  // All good
+        }
+        // In all other cases:
+        deactivate_plugins( plugin_basename( __FILE__ ) ); 
+        unset($_GET['activate']);  // No green success message
+    }
+
+    static function get_newsletter_plugin ()
+    {
+        foreach (get_plugins() as $path => $info) {
+            if ($info["PluginURI"] ===
+                "https://www.thenewsletterplugin.com/plugins/newsletter") {
+                return $path;
+            }
+        }
+        return null;
+    }
+
+    static function add_admin_notice($methodname) {
+        add_action('admin_notices', array(get_called_class(), $methodname));
+    }
+
+    static function error_need_newsletter_plugin ()
+    {
+        self::error_box(
+            ___( 'EPFL-newsletter activation failed' ),
+            ___( 'EPFL-newsletter requires <a href="https://www.thenewsletterplugin.com/">the newsletter plugin</a> to be installed and activated.' ));
+    }
+
+    static function error_newsletter_plugin_deactivated_cannot_activate ()
+    {
+        self::error_box(
+            ___( 'EPFL-newsletter activation failed' ),
+            ___('Please activate The Newsletter Plugin first.'));
+    }
+
+    static function notice_newsletter_plugin_deactivated_must_deactivate ()
+    {
+        echo "<div class=\"updated notice is-dismissible\"><p>\n";
+        echo ___("Automatically deactivated EPFL-newsletter");
+        echo "\n</p></div>\n";
+    }
+
+    static function error_box($title, $text) {
+        echo "<div class=\"error\">\n";
+        echo "<h1>$title</h1>\n";
+        echo "<p>\n";
+        echo $text;
+        echo "\n</p></div>\n";
+
     }
 }
 
