588adec3ac8a7f357deea9183250afcb4a7f837d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2018-01-23 14:51:02 +0100

    Download all images so as to embed them
    * New ImageCache and ImageCacheException classes
    * Cache to disk (so as to speed up sending the newsletter)

--- inc/EPFLMailer.php
+++ inc/EPFLMailer.php
@@ -86,22 +86,19 @@ class EPFLMailer
     }
 
     /**
-     * Make image links relative again, so that PHPMailer::msgHTML
-     * can embed the images.
-     */
-    /* It is just simpler and more robust to post-process the img
-     * links in this way, rather than to engage in a crusade to teach
-     * HTML editors in use with the newsletter plugin (of which there
-     * is no less than three) how the Web actually works.
+     * Download all images, so that PHPMailer::msgHTML
+     * can embed them.
      */
     function massage_html ($html) {
+        @file_put_contents(WP_CONTENT_DIR . "/newsletter.html", $html);  // XXX
          $doc = new \DOMDocument();
 
-         // Unless told otherwise, DOMDocument::loadHTML() assumes
-         // iso-8859-1 (https://stackoverflow.com/a/8218649/435004).
-         // Again, rather than trusting the HTML editors to preserve a
-         // <meta charset="UTF-8"> at the beginning of the string,
-         // inject or update an XML prolog to clue DOMDocument in.
+         /* Unless told otherwise, DOMDocument::loadHTML() assumes
+          * iso-8859-1 (https://stackoverflow.com/a/8218649/435004).
+          * Rather than trusting WordPress' HTML editors (of which
+          * there are no less than three) to preserve a <meta
+          * charset="UTF-8"> at the beginning of the string, inject or
+          * update an XML prolog to clue DOMDocument in. */
          $xml_prolog = array();
          preg_match("/^(<\?xml.*?\?>)/s", trim($html), $xml_prolog);
          $html = preg_replace("/^(<\?xml.*?\?>)/s", "", trim($html));
@@ -121,14 +118,160 @@ class EPFLMailer
          if (! preg_match('/\/$/', $base)) {
              $base = $base . "/";
          }
+         // '; # ";  #emacswhatever
+
          foreach ($doc->getElementsByTagName("img") as $img) {
+             if ($img->getAttribute("height") == 1 &&
+                 $img->getAttribute("width") == 1) {
+                 continue;  // Tracker pixel, ignore
+             }
              $src = $img->getAttribute("src");
-             if (0 === strpos($src, $base)) {
-                 $src = substr($src, strlen($base));
-                 $img->setAttribute("src", $src);
+             if (preg_match("#^https?:#", $src)) {
+                 try {
+                     $img->setAttribute("src", ImageCache::get($src)->as_relative_url());
+                 } catch (ImageCacheException $e) {
+                     static $has_complained_already = array();
+                     if (! $has_complained_already[$src]) {
+                         error_log("ImageCacheException for $src: $e");
+                         $has_complained_already[$src] = true;
+                     }
+                 }
              }
          }
          return $doc->saveHTML();
+    }
+}
+
+class ImageCacheException extends \Exception { }
+
+/**
+ * A cache of downloaded images that go into a newsletter
+ */
+class ImageCache {
+    static function get ($url) {
+        $thisclass = get_called_class();
+        return new $thisclass($url);
+    }
+
+    private function __construct ($url)
+    {
+        $this->url = $url;
+
+        if (! $this->_find_on_disk()) {
+            $this->_save_to_disk($this->_fetch());
+        }
+    }
+
+    private function _stem ()
+    {
+        return hash("sha256", $this->url);
+    }
+
+    private function _cache_dir ()
+    {
+        if (! $this->_cache_dir) {
+            $cache_dir = WP_CONTENT_DIR . "/cache/epfl-newsletter-images";
+            if (file_exists($cache_dir)) {
+                if (! is_dir($cache_dir)) {
+                    throw new ImageCacheException("$cache_dir is not a directory");
+                }
+            } else {
+                if (! @mkdir($cache_dir, 0777, /* $recursive = */ true)) {
+                    throw new ImageCacheException("Cannot mkdir(\"$cache_dir\"): " . error_get_last());
+                }
+            }
+            $this->_cache_dir = $cache_dir;
+        }
+        return $this->_cache_dir;
+    }
+
+    const IMAGE_EXTS = array(
+        IMG_PNG  => "png",
+        IMG_GIF  => "gif",
+        IMG_JPG  => "jpg",
+        IMG_PNG  => "png",
+        IMG_WBMP => "bmp",
+        IMG_XPM  => "xpm",
+        IMG_WEBP => "webp"
+    );
+
+    private function _get_known_extensions () {
+        $image_types_bitmap = \imagetypes();
+        $extensions = array();
+        
+        foreach (static::IMAGE_EXTS as $bit => $ext) {
+            if ($image_types_bitmap & $bit) {
+                array_push($extensions, $ext);
+            }
+        }
+        return $extensions;
+    }
+
+    private function _find_on_disk ()
+    {
+        if (! $this->_path_on_disk) {
+            foreach ($this::_get_known_extensions() as $ext) {
+                $candidate_path = $this->_make_path_on_disk($ext);
+                if (! file_exists($candidate_path)) { continue; }
+
+                $this->_path_on_disk = $candidate_path; break;
+            }
+        }
+        return $this->_path_on_disk;
+    }
+
+    private function _make_path_on_disk ($ext = null) {
+        $stem = sprintf("%s/%s",$this->_cache_dir(), $this->_stem());
+        if ($ext) {
+            return "$stem.$ext";
+        } else {
+            return $stem;
+        }
+    }
+
+    private function _save_to_disk ($blob) {
+        // https://bugs.php.net/bug.php?id=65187, #lesigh
+        $filename_sans_ext = $this->_make_path_on_disk();
+        if (! @file_put_contents($filename_sans_ext, $blob)) {
+            throw new ImageCacheException("Cannot create $filename_sans_ext: " . error_get_last());
+        }
+
+        $exif_image_type = exif_imagetype($filename_sans_ext);
+        if (false === $exif_image_type) {
+            // I'm not even sure whether error_get_last() is the proper
+            // channel here, #relesigh
+            throw new ImageCacheException("Unable to decode: " . error_get_last());
+        }
+        $ext = IMAGE_EXTS[$exif_image_type];
+        if (! $ext) {
+            throw new ImageCacheException("Unknown EXIF type: $exif_image_type");
+        }
+        $full_filename = $this->_make_path_on_disk($ext);
+        if (! @rename($filename_sans_ext, $full_filename)) {
+            throw new ImageCacheException("Cannot rename $filename_sans_ext to $full_filename: " . error_get_last());
+        }
+
+        $this->_path_on_disk = $full_filename;
+        return $full_filename;
+    }
+
+    private function _fetch () {
+        $curl = curl_init($this->url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        $error = curl_error($curl);
+        curl_close($curl);
+
+        if ($data === false) {
+            throw new ImageCacheException("Unable to fetch $url: " . $error);
+        }
+
+        return $data;
+    }
+
+    function as_relative_url() {
+        return sprintf("wp-content/cache/epfl-newsletter-images/%s",
+                       basename($this->_find_on_disk()));
     }
 }
 
